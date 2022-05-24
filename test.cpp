16f1070f42321e0547d1195833d64b0bdb7c78ee
Prince Bansal <prince.bansal008@gmail.com>
2022-05-24 05:39:42 +0530

    bits/stdc++.h precompiled on std-c++17 and code runner setting updated

--- test.cpp
+++ test.cpp
@@ -1,21 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 #define nl "\n"
 
 int main()
 {
+    vector<int> v { 1, 2, 3, 4, 50 };
 
-    if (__cplusplus == 202002L)
-        cout << "C++20\n";
-    else if (__cplusplus == 201703L)
-        cout << "C++17\n";
-    else if (__cplusplus == 201402L)
-        cout << "C++14\n";
-    else if (__cplusplus == 201103L)
-        cout << "C++11\n";
-    else if (__cplusplus == 199711L)
-        cout << "C++98\n";
-    else
-        cout << "pre-standard C++\n";
+    for (int x : v)
+        cout << x << " ";
+    cout << nl;
+    cout << "precompiled bits/stdc++" << nl;
     return 0;
 }
