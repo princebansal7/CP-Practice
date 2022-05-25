17190a9e910ae56ec8106502fc8a54e131d06c5e
Prince Bansal <prince.bansal008@gmail.com>
2022-05-26 04:58:22 +0530

    O(1) solution added

--- Level-1/1.CPP-Basic-1/8.ThreeNumbers.cpp
+++ Level-1/1.CPP-Basic-1/8.ThreeNumbers.cpp
@@ -21,5 +21,48 @@ int main()
         }
     }
     cout << cnt;
+
+    /* WAY-2
+        int k, s;
+        cin >> k >> s;
+
+        int count = 0;
+        for (int x = 0; x <= k; x++) {
+            for (int y = 0; y <= k; y++) {
+                int z = s - (x + y);
+                if (z < 0 || z > k)
+                    continue;
+
+                count++;
+            }
+        }
+
+        cout << count << endl;
+    */
+
+    /* O(1) Solution
+
+        int k, s;
+        cin >> k >> s;
+
+        if (k > s) {
+            cout << (s + 1) * (s + 2) / 2 << endl;
+            return 0;
+        }
+
+        if (3 * k < s) {
+            cout << 0 << endl;
+            return 0;
+        }
+
+        if (2 * k <= s) {
+            int r = 3 * k - s + 1;
+            cout << r * (r + 1) / 2;
+            return 0;
+        }
+
+        int n = k - 1, m = s - k;
+        cout << (k + 1) * (k + 2) / 2 + (n - m + 1) * m << endl;
+    */
     return 0;
 }
