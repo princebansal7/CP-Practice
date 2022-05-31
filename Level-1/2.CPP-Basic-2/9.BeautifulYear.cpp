85629f00fbeb96bae67f7f8dbb722c0e7fd1c87c
Prince Bansal <prince.bansal008@gmail.com>
2022-06-01 03:51:06 +0530

    file added

---
+++ Level-1/2.CPP-Basic-2/9.BeautifulYear.cpp
@@ -0,0 +1,27 @@
+/*
+    @author: princebansal_
+*/
+
+#include <iostream>
+using namespace std;
+#define nl '\n'
+#define ll long long
+
+int main()
+{
+    int year;
+    cin >> year;
+    while (true) {
+        year++;
+        int fDigit = year / 1000;
+        int SDigit = (year / 100) % 10;
+        int TDigit = (year / 10) % 10;
+        int LDigit = year % 10;
+
+        if (fDigit != SDigit && fDigit != TDigit && fDigit != LDigit && SDigit != TDigit && SDigit != LDigit && TDigit != LDigit) {
+            cout << year << nl;
+            break;
+        }
+    }
+    return 0;
+}
