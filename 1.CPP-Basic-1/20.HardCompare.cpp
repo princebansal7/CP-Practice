5378730cbe69bedc9e50f1b31916a4e0f0bf6570
Prince Bansal <prince.bansal008@gmail.com>
2022-06-06 14:30:13 +0530

    correct code added

--- 1.CPP-Basic-1/20.HardCompare.cpp
+++ 1.CPP-Basic-1/20.HardCompare.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 #define nl '\n'
 #define ll long long
@@ -11,6 +12,17 @@ using namespace std;
 
 int main()
 {
+    // Approach:
+    //    a^b > c^d
+    // => log(a^b) > log(c^d)
+    // => b*log(a) > d*log(c)
+    // NOTE: log(x) => log(x) with base e
+    //       log10(x) => with base 10
+    //       log2(x) => with base 2
+
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    (b * log(a) > d * log(c)) ? cout << "YES" << nl : cout << "NO" << nl;
 
     return 0;
 }
