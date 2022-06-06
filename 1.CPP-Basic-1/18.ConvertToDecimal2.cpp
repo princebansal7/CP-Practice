5115b53a9e7c934fd5d5dd36e950fa5f6ce094ed
Prince Bansal <prince.bansal008@gmail.com>
2022-06-06 16:44:32 +0530

    updated

--- 1.CPP-Basic-1/18.ConvertToDecimal2.cpp
+++ 1.CPP-Basic-1/18.ConvertToDecimal2.cpp
@@ -2,6 +2,8 @@
     @author: princebansal_
 */
 
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 using namespace std;
 #define nl '\n'
@@ -9,8 +11,49 @@ using namespace std;
 #define repn(i, a, b) for (int i = a; i <= b; ++i)
 #define rep(i, a, b) for (int i = a; i < b; ++i)
 
+string decimalToBinary(int n)
+{
+    string ans;
+    while (n > 0) {
+        int bit = n % 2;
+        ans.push_back(bit + 48);
+        n = n >> 1; // dividing  by 2
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+int powe(int n, int power)
+{
+    int ans;
+    for (int i = 1; i <= power; i++) {
+        ans = n * power;
+    }
+    return ans;
+}
+
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string binary = decimalToBinary(n);
+        // cout << binary << nl;
+
+        int power = 1, finalNum = 0;
+        for (int i = binary.size() - 1; i >= 0; i--) {
+
+            int bit = (binary[i] - 48);
+
+            if (bit == 1) {
+                finalNum += power;
+                power = powe(power, 2);
+            }
+        }
+        cout << finalNum << nl;
+    }
 
     return 0;
 }
