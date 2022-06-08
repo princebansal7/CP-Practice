dd794f2b596d2563eaf79fa9cddc5642d1d7b410
Prince Bansal <prince.bansal008@gmail.com>
2022-06-08 19:06:29 +0530

    file added

--- 3.Time-Complexity-Analysis/2.SystemOfEquations.cpp
+++ 3.Time-Complexity-Analysis/2.SystemOfEquations.cpp
@@ -11,6 +11,21 @@ using namespace std;
 
 int main()
 {
-
+    int n, m;
+    cin >> n >> m;
+    int cnt = 0;
+    int till = min(n, m);
+    repn(i, 0, till)
+    {
+        repn(j, 0, till)
+        {
+            if ((i * i + j == n) and (i + j * j == m)) {
+                // cout << "IN:" << i << " " << j << nl;
+                cnt++;
+            }
+            // cout << "OUT:" << i << " " << j << nl;
+        }
+    }
+    cout << cnt << nl;
     return 0;
 }
