7037d19728a2d7bf0d74e276795f78c713050595
Prince Bansal <prince.bansal008@gmail.com>
2022-06-08 16:47:53 +0530

    file added

--- 3.Time-Complexity-Analysis/1.FairPlayoff.cpp
+++ 3.Time-Complexity-Analysis/1.FairPlayoff.cpp
@@ -11,6 +11,19 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int m1 = max(a, b);
+        int m2 = max(c, d);
+
+        if (m1 > min(c, d) and m2 > min(a, b))
+            cout << "YES" << nl;
+        else
+            cout << "NO" << nl;
+    }
 
     return 0;
 }
