45ab6baa954ac0e26fee044d6975ab5b583279ac
Prince Bansal <prince.bansal008@gmail.com>
2022-06-09 01:22:33 +0530

    file added

--- 3.Time-Complexity-Analysis/3.MagicNumbers.cpp
+++ 3.Time-Complexity-Analysis/3.MagicNumbers.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 #define nl '\n'
 #define ll long long
@@ -11,6 +12,23 @@ using namespace std;
 
 int main()
 {
-
+    int n;
+    cin >> n;
+    bool ok = true;
+    while (n > 0) {
+        if (n % 10 == 1) {
+            n /= 10;
+        } else if (n % 100 == 14) {
+            n /= 100;
+        } else if (n % 1000 == 144) {
+            n /= 1000;
+        } else {
+            cout << "NO" << nl;
+            ok = false;
+            break;
+        }
+    }
+    if (ok)
+        cout << "YES" << nl;
     return 0;
 }
