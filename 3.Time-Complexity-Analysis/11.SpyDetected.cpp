82af5a1cdd32cc20d0a9f6109d086a121f9578d0
Prince Bansal <prince.bansal008@gmail.com>
2022-06-11 00:31:55 +0530

    file added

---
+++ 3.Time-Complexity-Analysis/11.SpyDetected.cpp
@@ -0,0 +1,43 @@
+/*
+    @author: princebansal_
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define ll long long
+#define repn(i, a, b) for (int i = a; i <= b; ++i)
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+int main()
+{
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        unordered_map<int, int> mp;
+        rep(i, 0, n)
+        {
+            cin >> v[i];
+            mp[v[i]]++;
+        }
+        int val, ans;
+        for (auto x : mp) {
+            if (x.second == 1) {
+                val = x.first;
+                break;
+            }
+        }
+        rep(i, 0, n)
+        {
+            if (val == v[i]) {
+                ans = i;
+                break;
+            }
+        }
+        cout << ans + 1 << nl;
+    }
+    return 0;
+}
